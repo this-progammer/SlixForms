7be126ee90d14545650237ed20dc2c38fb8f274e
*(this)-programmer == i++ <140750964+this-progammer@users.noreply.github.com>
2026-07-23 14:24:54 -0500

    Create SlixFormsCore.h

---
+++ core/SlixFormsCore.h
@@ -0,0 +1,23 @@
+/*SlixFormsCore.h*/
+
+#ifndef SLIX_FORMS_CORE_
+#define SLIX_FORMS_CORE_
+
+/*SlixFormsVersion*/
+#ifdef SLIX_FORMS_CORE_
+#define SLIX_FORMS_VERSION 1
+#endif
+
+namespace Slix 
+{
+      mutable class Core {
+        public:
+          Core();
+          ~Core();
+          
+          void Initalize();
+
+      };
+}
+
+#endif
